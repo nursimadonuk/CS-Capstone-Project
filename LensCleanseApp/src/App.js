b3ab611065b0b8ecf9bd18b1e315c9141d7a3d8a
Nursima <40703365+nursimadonuk@users.noreply.github.com>
2021-09-20 11:42:08 -0400

    Rough Front End

--- LensCleanseApp/src/App.js
+++ LensCleanseApp/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Lens Cleanse!</h1>
+    <div className="app">
+
+      <div className="app_header">
+        <img
+          className="app_header_image"
+          // place holder
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlyhZtrjdFZkkTpR6ludPBtdB3poErWgqGUQ&usqp=CAU"
+          // src="/Users/nursimadonuk/Desktop/LensCleansApp/lens-cleanse/logo.jpeg" 
+          alt=""
+        />
+
+      </div>
+
+      <h1>Welcome to Lens Cleanse!!</h1>
+
+      <Post username="NursimaDonuk" caption="First Post" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlyhZtrjdFZkkTpR6ludPBtdB3poErWgqGUQ&usqp=CAU"/>
+
+
     </div>
   );
 }

1ba091cfc47273b0564537f3a835209bdff3c792
Saadat Taaseen <saadat.taaseen@tred.earth>
2021-09-27 01:24:03 -0400

    added a ternary operator to make sure both description and images exist while uploading

--- LensCleanseApp/src/ImageUpload.js
+++ LensCleanseApp/src/ImageUpload.js
@@ -4,67 +4,74 @@ import { storage, collectPosts } from './firebase';
 import { ref, getDownloadURL } from 'firebase/storage';
 import { serverTimestamp, FieldValue } from 'firebase/firestore';
 
-function ImageUpload({username}) {
-    const [image, setImage] = useState(null);
-    const [progress, setProgress] = useState(0);
-    const [caption, setCaption] = useState('');
+function ImageUpload({ username }) {
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [caption, setCaption] = useState('');
 
-    const handleChange = (e) => {
-        if (e.target.files[0]) {
-            setImage(e.target.files[0]);
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    //************** NEEDS TO BE FIXED: put ***************/
+    if (!image || !caption) {
+      alert('PLEASE MAKE SURE YOU ENTERED BOTH IMAGE AND CAPTION')
+    }
+    else {
+      const uploadTask = ref(storage, `images/${image.name}`).put(image);
+      // const uploadTask = ref(storage, `images/${image.name}`);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // progress function
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(progress);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          // complete function
+          //************** NEEDS TO BE FIXED: child ***************/
+          getDownloadURL(ref(storage, "images").child(image.name))
+            .then(url => {
+              collectPosts.add({
+                timestamp: serverTimestamp(FieldValue),
+                caption: caption,
+                imageUrl: url,
+                username: username
+              });
+
+              setProgress(0);
+              setCaption("");
+              setImage(null);
+
+            })
+
         }
-    };
 
-    const handleUpload = () => {
-        //************** NEEDS TO BE FIXED: put ***************/
-        const uploadTask = ref(storage, `images/${image.name}`).put(image);
-        // const uploadTask = ref(storage, `images/${image.name}`);
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                // progress function
-                const progress = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
-                setProgress(progress);
-            },
-            (error) => {
-                console.log(error);
-                alert(error.message);
-            },
-            () => {
-                // complete function
-                //************** NEEDS TO BE FIXED: child ***************/
-                getDownloadURL(ref(storage, "images").child(image.name))
-                .then(url => {
-                    collectPosts.add({
-                        timestamp: serverTimestamp(FieldValue),
-                        caption: caption,
-                        imageUrl: url,
-                        username: username
-                    });
+      )
+    }
+  };
 
-                    setProgress(0);
-                    setCaption("");
-                    setImage(null);
+  return (
+    <div>
 
-                })
-            }
-        )
-    };
+      <progress className="imageupload_progress" value={progress} max="100" />
+      <input type="text" placeholder="Enter a caption..." onChange={event => setCaption(event.target.value)} value={caption} />
+      <input type="file" onChange={handleChange} />
+      <Button onClick={handleUpload}>
+        Upload
+      </Button>
 
-    return (
-        <div>
-
-            <progress className="imageupload_progress" value={progress} max="100" />
-            <input type="text" placeholder="Enter a caption..." onChange={event => setCaption(event.target.value)} value={caption}/>
-            <input type="file" onChange={handleChange}/>
-            <Button onClick={handleUpload}>
-                Upload
-            </Button>
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default ImageUpload
